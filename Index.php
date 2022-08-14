24bddb3a9243b16719835351c407fca54deb8aad
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-08-14 16:02:12 +0200

    Fixing broken code

---
+++ Index.php
@@ -0,0 +1,44 @@
+<?php
+
+//Broken code given on challenge
+
+name = "  Bob  ";
+
+age = "thirty five";
+
+$job = plumber;
+
+$carMake = " Toyota";
+
+$carColour = "RED";
+
+$happy = true
+
+$time = "this many";
+
+// Correct code to the challenge
+
+$name = "  Bob  ";          // Corrected added a $
+    echo $name;
+
+    echo trim($name);       // Added trim to remove space around Bob
+
+$agewas = "thirty five";    // Corrected added a $
+
+$age = "35"                 // Change age from letters to numbers
+    echo $age;
+
+$job = "plumber";           // Corrected added a " "
+    echo $job;
+
+$carMake = " Toyota";
+
+$carColour = "RED";
+    echo strtolower ($carColour)             // Capital letters to lower case
+
+$happy = "true";            // Corrected added a " " and ;
+    echo $happy;
+
+$time = "this many";
+
+?>
